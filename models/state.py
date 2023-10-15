638656a4775f67b3b289a530a1b0039d8195a335
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-15 17:25:27 +0100

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""classes that inherit from BaseModel State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """The name of the state"""
+
+    name = ""
