5c4dde01ad8da85dd394645964d72b2e1e9ed8ad
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-15 18:26:39 +0100

    Create test_amenity.py

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Unittest for amenity.py
+"""
+import unittest
+from models.amenity import Amenity
+import datetime
+
+class TestAmenity(unittest.TestCase):
+    def test_class_exists(self):
+        a = Amenity()
+        self.assertEqual(str(type(a)), "<class 'models.amenity.Amenity'>")
+
+    def test_amenity_inheritance(self):
+        a = Amenity()
+        self.assertIsInstance(a, Amenity)
+
+    def test_has_attributes(self):
+        a = Amenity()
+        self.assertTrue(hasattr(a, 'name'))
+        self.assertTrue(hasattr(a, 'id'))
+        self.assertTrue(hasattr(a, 'created_at'))
+        self.assertTrue(hasattr(a, 'updated_at'))
+
+    def test_attribute_types(self):
+        a = Amenity()
+        self.assertIsInstance(a.name, str)
+        self.assertIsInstance(a.id, str)
+        self.assertIsInstance(a.created_at, datetime.datetime)
+        self.assertIsInstance(a.updated_at, datetime.datetime)
+
+if __name__ == '__main__':
+    unittest.main()
