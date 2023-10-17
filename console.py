1e2c7ff0940744416fffd4ad2456c2646a39272e
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-17 22:37:41 +0100

    Update console.py

--- console.py
+++ console.py
@@ -2,13 +2,15 @@
 import cmd
 import re
 from shlex import split
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
+
 
 def parse(arg):
     """Parse a string of arguments and return a list of tokens"""
@@ -25,6 +27,7 @@ def parse(arg):
             break
     return tokens
 
+
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand command interpreter"""
 
@@ -34,8 +37,8 @@ class HBNBCommand(cmd.Cmd):
         "User": User,
         "State": State,
         "City": City,
-        "Amenity": Amenity,
         "Place": Place,
+        "Amenity": Amenity,
         "Review": Review
     }
 
@@ -46,93 +49,152 @@ class HBNBCommand(cmd.Cmd):
     def default(self, arg):
         """Handle unknown commands"""
         arg_parts = parse(arg)
-        if len(arg_parts) >= 2 and arg_parts[0] in self.classes:
+        if len(arg_parts) >= 4 and arg_parts[0] in self.classes:
             class_name = arg_parts[0]
             command = arg_parts[1]
-            if command == "all":
-                return self.do_all(class_name)
-            elif command == "count":
-                return self.do_count(class_name)
-            elif command == "show" and len(arg_parts) >= 3:
-                instance_id = arg_parts[2]
+            instance_id = arg_parts[2]
+            attribute = arg_parts[3]
+            if command == "show":
                 return self.do_show(f"{class_name} {instance_id}")
-            elif command == "destroy" and len(arg_parts) >= 3:
-                instance_id = arg_parts[2]
+            elif command == "destroy":
                 return self.do_destroy(f"{class_name} {instance_id}")
-            elif command == "update" and len(arg_parts) >= 6:
-                instance_id = arg_parts[2]
-                attribute = arg_parts[3]
+            elif command == "update" and len(arg_parts) >= 5:
                 value = arg_parts[4]
                 return self.do_update(f"{class_name} {instance_id} {attribute} {value}")
         print("*** Unknown syntax: {}".format(arg))
         return False
 
-    def do_all(self, arg):
-        """Display string representations of instances"""
-        arg_parts = parse(arg)
-        if len(arg_parts) == 1 and arg_parts[0] in self.classes:
-            class_name = arg_parts[0]
-            all_objs = self.storage.all()
-            obj_list = [str(obj) for obj in all_objs.values() if obj.__class__.__name__ == class_name]
-            print(obj_list)
-        else:
-            print("*** Unknown syntax: all")
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
 
-    def do_count(self, arg):
-        """Count instances of a class"""
+    def do_EOF(self, arg):
+        """Exit the program on EOF"""
+        print("")
+        return True
+
+    def do_create(self, arg):
+        """Create a new instance of a class and print its ID"""
         arg_parts = parse(arg)
-        if len(arg_parts) == 1 and arg_parts[0] in self.classes:
-            class_name = arg_parts[0]
-            all_objs = self.storage.all()
-            count = sum(1 for obj in all_objs.values() if obj.__class__.__name__ == class_name)
-            print(count)
+        if len(arg_parts) == 0:
+            print("** class name missing **")
+        elif arg_parts[0] not in self.classes:
+            print("** class doesn't exist **")
         else:
-            print("*** Unknown syntax: count")
+            new_instance = self.classes[arg_parts[0]]()
+            new_instance.save()
+            print(new_instance.id)
 
     def do_show(self, arg):
         """Show the string representation of an instance"""
         arg_parts = parse(arg)
-        if len(arg_parts) == 2 and arg_parts[0] in self.classes:
-            class_name, obj_id = arg_parts
-            key = f"{class_name}.{obj_id}"
-            all_objs = self.storage.all()
+        if len(arg_parts) == 0:
+            print("** class name missing **")
+        elif arg_parts[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(arg_parts) < 2:
+            print("** instance id missing **")
+        else:
+            obj_id = arg_parts[1]
+            key = f"{arg_parts[0]}.{obj_id}"
+            all_objs = storage.all()
             if key in all_objs:
                 print(all_objs[key])
             else:
                 print("** no instance found **")
-        else:
-            print("*** Unknown syntax: show")
 
     def do_destroy(self, arg):
         """Delete an instance by class name and ID"""
         arg_parts = parse(arg)
-        if len(arg_parts) == 2 and arg_parts[0] in self.classes:
-            class_name, obj_id = arg_parts
-            key = f"{class_name}.{obj_id}"
-            all_objs = self.storage.all()
+        if len(arg_parts) == 0:
+            print("** class name missing **")
+        elif arg_parts[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(arg_parts) < 2:
+            print("** instance id missing **")
+        else:
+            obj_id = arg_parts[1]
+            key = f"{arg_parts[0]}.{obj_id}"
+            all_objs = storage.all()
             if key in all_objs:
                 del all_objs[key]
-                self.storage.save()
+                storage.save()
             else:
                 print("** no instance found **")
+
+    def do_all(self, arg):
+        """Display string representations of instances"""
+        arg_parts = parse(arg)
+        if len(arg_parts) == 0:
+            all_objs = storage.all()
+            print([str(obj) for obj in all_objs.values()])
+        elif arg_parts[0] not in self.classes:
+            print("** class doesn't exist **")
         else:
-            print("*** Unknown syntax: destroy")
+            all_objs = storage.all()
+            obj_list = [str(obj) for obj in all_objs.values() if obj.__class__.__name__ == arg_parts[0]]
+            print(obj_list)
+
+    def do_count(self, arg):
+        """Count instances of a class"""
+        arg_parts = parse(arg)
+        if len(arg_parts) == 0:
+            print("** class name missing **")
+        elif arg_parts[0] not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            all_objs = storage.all()
+            count = sum(1 for obj in all_objs.values() if obj.__class__.__name__ == arg_parts[0])
+            print(count)
 
     def do_update(self, arg):
         """Update an instance's attributes"""
         arg_parts = parse(arg)
-        if len(arg_parts) == 4 and arg_parts[0] in self.classes:
-            class_name, obj_id, attribute, value = arg_parts
-            key = f"{class_name}.{obj_id}"
-            all_objs = self.storage.all()
+        if len(arg_parts) == 0:
+            print("** class name missing **")
+        elif arg_parts[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(arg_parts) < 2:
+            print("** instance id missing **")
+        else:
+            obj_id = arg_parts[1]
+            key = f"{arg_parts[0]}.{obj_id}"
+            all_objs = storage.all()
             if key in all_objs:
                 obj = all_objs[key]
-                setattr(obj, attribute, value)
-                obj.save()
+                if len(arg_parts) < 3:
+                    print("** attribute name missing **")
+                elif len(arg_parts) < 4:
+                    print("** value missing **")
+                else:
+                    attr_name = arg_parts[2]
+                    attr_value = arg_parts[3]
+                    setattr(obj, attr_name, attr_value)
+                    obj.save()
             else:
                 print("** no instance found **")
+
+    def do_User(self, arg):
+        """Handle User commands"""
+        args = parse(arg)
+        if len(args) < 1:
+            print("** Missing command for User")
+            return
+
+        command = args[0]
+        if command == "all":
+            self.do_all("User")
+        elif command == "count":
+            self.do_count("User")
+        elif command == "show" and len(args) > 1:
+            self.do_show("User " + args[1])
+        elif command == "destroy" and len(args) > 1:
+            self.do_destroy("User " + args[1])
+        elif command == "update" and len(args) > 3:
+            self.do_update("User " + ' '.join(args[1:]))
         else:
-            print("*** Unknown syntax: update")
+            print("** Unknown command: User." + command)
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
